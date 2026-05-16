95640d96f95523a100b215a041662a64775c1806
MD. Mohaiminul Haque <tashinggara129@gmail.com>
2026-05-17 00:22:02 +0600

    Restore default database connection

--- config/db.php
+++ config/db.php
@@ -1,11 +1,10 @@
 <?php
-define("DB_HOST", "127.0.0.1");
+define("DB_HOST", "localhost");
 define("DB_USER", "root");
 define("DB_PASS", "");          // leave blank unless you set a MySQL password in XAMPP
 define("DB_NAME", "ecommerce_store");
-define("DB_PORT", 3307);
  
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
  
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
